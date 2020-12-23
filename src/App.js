a3f5924c8239e5ff8f09b1fd30ff71342464156b
Park Jeongyong <kladess@gmail.com>
2020-12-23 13:58:40 +0900

    import React from "react"; import React from "react";

--- src/App.js
+++ src/App.js
@@ -1,64 +1,69 @@
-import { createState, useState } from '@hookstate/core';
-import './App.css';
+import React from "react";
+import { createState, useState } from "@hookstate/core";
+import "./App.css";
 
 const sharedIntState = createState(0);
-const sharedListState = createState([{ name: 'admin', dt: new Date().getTime() }]);
+const sharedListState = createState([
+  { name: "admin", dt: new Date().getTime() }
+]);
 
 export default function App() {
-
-  return <>
-    <div className="App">
-      <DisplayIntPanel />
-      <ControlIntPanel />
-      <hr />
-      <ControlListPanel />
-      <DisplayListPanel />
-
-    </div>
-  </>;
+  return (
+    <>
+      <div className="App">
+        <DisplayIntPanel />
+        <ControlIntPanel />
+        <hr />
+        <ControlListPanel />
+        <DisplayListPanel />
+      </div>
+    </>
+  );
 }
 
 const DisplayIntPanel = () => {
   const state = useState(sharedIntState);
-  return <>
-    <b>Counter value: {state.get()} </b>
-  </>
-}
+  return (
+    <>
+      <b>Counter value: {state.get()} </b>
+    </>
+  );
+};
 
 function ControlIntPanel() {
-
   const state = useState(sharedIntState);
 
   const handerIncrement = () => {
-    state.set(p => p + 1)
-  }
+    state.set((p) => p + 1);
+  };
 
-  return <>
-    <button onClick={handerIncrement}> Increment</button>
-  </>;
+  return (
+    <>
+      <button onClick={handerIncrement}> Increment</button>
+    </>
+  );
 }
 
 function DisplayListPanel() {
   const listState = useState(sharedListState);
 
-  return <div>
-    <ul>
-      {listState.map((entityState, idx) => {
-
-
-        return <EntityViewer entityState={entityState} />
-      })}
-    </ul>
-  </div>
+  return (
+    <div>
+      <ul>
+        {listState.map((entityState, idx) => {
+          return <EntityViewer entityState={entityState} />;
+        })}
+      </ul>
+    </div>
+  );
 }
 function EntityViewer({ entityState }) {
   const entity = useState(entityState);
 
   const { name, dt } = entity.value;
 
-
   const str = `${name}, ${dt}`;
-  return <li>{str}</li>
+  return <li>{str}</li>;
 }
 
 function ControlListPanel() {
@@ -69,13 +74,14 @@ function ControlListPanel() {
     const newItem = {
       name,
       dt
-    }
-    listState.set(nodes => (nodes || []).concat([newItem]));
+    };
+    listState.set((nodes) => (nodes || []).concat([newItem]));
     // listState.set(nodes => (nodes || []).push(newItem)); 안됨
+  };
 
-  }
-
-  return <>
-    <button onClick={handerIncrement}>Increment</button>
-  </>;
+  return (
+    <>
+      <button onClick={handerIncrement}>Increment</button>
+    </>
+  );
 }
