bb15725cfd511c5ae31dfac726b7b91c91d0c3b5
Jeongyong Park <jypark.inno@gmail.com>
2021-01-04 11:23:39 +0900

    아이콘

---
+++ src/app/main/libs/icons/index.js
@@ -0,0 +1,7 @@
+import BugIcon from './BugIcon';
+import PineTreeIcon from './PineTreeIcon';
+import RadishIcon from './RadishIcon';
+import RadishLineIcon from './RadishLineIcon';
+import SeaWeedIcon from './SeaWeedIcon';
+
+export { BugIcon, PineTreeIcon, RadishIcon, RadishLineIcon, SeaWeedIcon };
